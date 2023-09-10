b527a5f28ac25d1077af9418cf89ba545f159b48
Maleki <saj.maleki@gmail.com>
2023-09-10 16:07:07 +0330

    intitialilza chapter 4

---
+++ 04-Conceptuals/final/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//__________JS Engine and Runtime__________
+
+//________________Scoping__________________
+
+//_______Hoisting and Temporal Dead Zone_____
+
+//____________The this Keyword_____________
+
+//______Regular Functions vs. Arrow Functions________
+
+//__________ Objects vs. primitives___________
