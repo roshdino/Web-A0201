d5ec5e3839b1111e0249e63fd4a76f95aa18b301
Sajjad <saj.maleki@gmail.com>
2023-09-10 17:49:20 +0330

    session 8

--- 04-Conceptuals/final/script.js
+++ 04-Conceptuals/final/script.js
@@ -2,6 +2,55 @@
 
 //__________JS Engine and Runtime__________
 
+// const number = 10;
+
+const logger = function () {
+  const firstName = "ali";
+  console.log(firstName);
+};
+
+// console.log(logger);
+
+// if (true) {
+//   console.log("first");
+// }
+
+// logger();
+
+// console.log("start");
+
+// setTimeout(logger, 2000);
+
+// setTimeout(() => {
+//   console.log('3 secconds passed')
+// }, 3000);
+
+const print_first = () => {
+  console.log("first");
+};
+
+const print_second = () => {
+  setTimeout(() => {
+    console.log("second");
+  }, 3000);
+};
+
+const print_third = () => {
+  console.log("third");
+};
+
+print_first();
+print_second();
+print_third();
+
+/*
+first
+second
+third
+
+
+*/
+
 //________________Scoping__________________
 
 //_______Hoisting and Temporal Dead Zone_____
