d7091f880888f43a8130f715175a76a55a4e0d42
Maleki <saj.maleki@gmail.com>
2023-09-20 16:00:00 +0330

    initializa chapter 6

---
+++ 06- More About Functions/final/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//---------------------Default Parameters---------------------
+
+
+//--------- Functions Accepting Callback Functions------------
+
+
+//---------------- Functions Returning Functions--------------
+
+
+//---------------- The call and apply Methods--------------
+
+
+//--------------------- The bind Method--------------------
+
+
+//-------Immediately Invoked Function Expressions (IIFE)----
+
+
+//------------------------- Closures------------------------
+
+
